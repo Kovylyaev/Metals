9fbdf16bad3fab068e7ee16e14d005449ed3241b
KovylyaevHSE <sasha_kovylyaev@mail.ru>
2025-04-22 13:40:12 +0300

    raw code in notebook

---
+++ setup.py
@@ -0,0 +1,21 @@
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+
+def main():
+    reqs = [str(req) for req in parse_requirements(open('requirements.txt'))]
+
+    setup(
+        name='Metals',
+        version='1.0.1',
+        author='Kovylyaev',
+        description='Carbon regr in metals micros',
+        packages=find_packages(),
+        python_requires='>=3.12',
+        url="https://github.com/Kovylyaev/Metals",
+        install_requires=reqs,
+    )
+
+
+if __name__ == '__main__':
+    main()
